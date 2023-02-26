a263bfb21fb70c4b06198b9333b384ca8bef2665
traveller195 <theo92@gmx.net>
2023-02-26 18:42:51 +0100

    Adaption data container use, construction, type tsx + new dataFields configs and in API + SQL add column in database table + translation of SQL Enum into german

--- app/src/frontend/building/data-containers/type.tsx
+++ app/src/frontend/building/data-containers/type.tsx
@@ -22,7 +22,7 @@ const AttachmentFormOptions = [
 const TypeView: React.FunctionComponent<CategoryViewProps> = (props) => {
     return (
         <Fragment>
-            <DataEntry
+{/*             <DataEntry
                 title="Base type classification"
                 slug=""
                 value=""
@@ -43,21 +43,58 @@ const TypeView: React.FunctionComponent<CategoryViewProps> = (props) => {
                 mode={props.mode}
                 onChange={props.onChange}
                 disabled={true}
-            />
+            /> */}
+
+
             <SelectDataEntry
                 title={dataFields.size_roof_shape.title}
                 slug="size_roof_shape"
                 value={props.building.size_roof_shape}
+                tooltip={dataFields.size_roof_shape.tooltip}
                 mode={props.mode}
                 copy={props.copy}
                 onChange={props.onChange}
                 disabled={true}
-                options={[
-                    "Flat",
-                    "Pitched",
-                    "Other"
-                ]}
+                options={dataFields.size_roof_shape.items}
             />
+
+            <Verification
+                slug="size_roof_shape"
+                allow_verify={props.user !== undefined && props.building.size_roof_shape !== null && !props.edited}
+                onVerify={props.onVerify}
+                user_verified={props.user_verified.hasOwnProperty("size_roof_shape")}
+                user_verified_as={props.user_verified.size_roof_shape}
+                verified_count={props.building.verified.size_roof_shape}
+            />   
+
+            <DataEntry
+                title={dataFields.size_roof_shape_source.title}
+                tooltip={dataFields.size_roof_shape_source.tooltip}
+                slug="size_roof_shape_source"
+                value={props.building.last_renovation_source}
+                mode={props.mode}
+                disabled={true}
+                copy={props.copy}
+                onChange={props.onChange}
+            />
+            <Verification
+                slug="last_renovation_source"
+                allow_verify={props.user !== undefined && props.building.last_renovation_source !== null && !props.edited}
+                onVerify={props.onVerify}
+                user_verified={props.user_verified.hasOwnProperty("last_renovation_source")}
+                user_verified_as={props.user_verified.last_renovation_source}
+                verified_count={props.building.verified.last_renovation_source}
+            />
+
+
+
+
+
+
+
+
+
+
             <SelectDataEntry
                 title={dataFields.building_attachment_form.title}
                 slug="building_attachment_form"
@@ -75,7 +112,56 @@ const TypeView: React.FunctionComponent<CategoryViewProps> = (props) => {
                 user_verified={props.user_verified.hasOwnProperty("building_attachment_form")}
                 user_verified_as={props.user_verified.building_attachment_form}
                 verified_count={props.building.verified.building_attachment_form}
-                />
+            />
+
+
+
+            <SelectDataEntry
+                title={dataFields.building_owner.title}
+                slug="building_owner"
+                value={props.building.building_owner}
+                mode={props.mode}
+                copy={props.copy}
+                onChange={props.onChange}
+                tooltip={dataFields.building_owner.tooltip}
+                placeholder={dataFields.building_owner.example}
+                options={dataFields.building_owner.items}
+            />
+            <Verification
+                slug="building_owner"
+                allow_verify={props.user !== undefined && props.building.building_owner !== null && !props.edited}
+                onVerify={props.onVerify}
+                user_verified={props.user_verified.hasOwnProperty("building_owner")}
+                user_verified_as={props.user_verified.building_owner}
+                verified_count={props.building.verified.building_owner}
+            />
+
+            <DataEntry
+                title={dataFields.building_owner_source.title}
+                tooltip={dataFields.building_owner_source.tooltip}
+                slug="building_owner_source"
+                value={props.building.building_owner_source}
+                mode={props.mode}
+                disabled={true}
+                copy={props.copy}
+                onChange={props.onChange}
+            />
+            <Verification
+                slug="building_owner_source"
+                allow_verify={props.user !== undefined && props.building.building_owner_source !== null && !props.edited}
+                onVerify={props.onVerify}
+                user_verified={props.user_verified.hasOwnProperty("building_owner_source")}
+                user_verified_as={props.user_verified.building_owner_source}
+                verified_count={props.building.verified.building_owner_source}
+            />
+
+
+
+
+
+
+
+
 
 {/*             <DataEntry
                 title="Local typology mutations"
