1a86156e5f4bf53cbe49952e920d97111e9e7b70
traveller195 <theo92@gmx.net>
2023-03-03 10:09:43 +0100

    Change welcome text tsx

--- app/src/frontend/pages/welcome.tsx
+++ app/src/frontend/pages/welcome.tsx
@@ -12,23 +12,23 @@ const Welcome = () => (
         <Categories mode="view"/>
         <h1 className="h2">Willkommen bei Colouring {config.cityName}!</h1>
         <p>
-            Wie alt sind die Gebäude Dresdens? Aus welchen Materialien wurden sie überwiegend erbaut? Und wie gut sind sie baulich vorbereitet, um mit extremen Ereignissen wir Starkregen, Hochwasser oder Hitze umgehen zu können?
+            Wie alt sind die Gebäude Dresdens? Aus welchen Materialien wurden sie überwiegend erbaut? Und wie gut sind sie baulich vorbereitet, um mit extremen Ereignissen wie Starkregen, Hochwasser oder Hitze umgehen zu können?
         </p>
         <p>
-            Um solche oder weitere Fragen beantworten zu können, soll Wissen zu den Gebäuden der Stadt {config.cityName} für unterschiedliche Gebäudemerkmale in einer offenen Wissensplattform Colouring {config.cityName} erfasst, 
+            Um solche und weitere Fragen beantworten zu können, soll Wissen zu den Gebäuden der Stadt {config.cityName} für verschiedene Merkmale in einer offenen Wissensplattform erfasst, 
             in Karten visualisiert und als offene Daten bereitgestellt werden.
             
-            Falls Sie Interesse an den Gebäuden der Stadt {config.cityName} haben und Ihr Wissen in einer interaktiven Karte erfassen und teilen möchten, dann sind Sie auf der Plattform Colouring {config.cityName} genau richtig!
+            Sie interessieren sich für die Gebäude der Stadt {config.cityName} haben und möchten Ihr Wissen in einer interaktiven Karte erfassen und teilen? Dann sind Sie auf der Plattform "Colouring {config.cityName}" genau richtig!
         </p>
         <p>
             <b>Wie kann ich mitmachen?</b>
         </p>
         <p>
-            Sie können individuell beginnen und Gebäudemerkmale eintragen - egal ob am heimischen Computer oder draußen an der frischen Luft. Einfach einen neuen Account registieren und loslegen.
-            Weiterhin werden unterschiedliche Veranstaltungen wie Vorträge, Spaziergänge ("Mapathons") oder ein (digitaler) monatlicher Stammtisch zum Erfahrungsaustausch angeboten: informieren Sie sich dazu auf der <a href="https://colouring.dresden.ioer.info" target="_blank">Projektwebseite</a>.
+            Sie können individuell beginnen und Gebäudemerkmale eintragen - egal ob am heimischen Computer oder draußen an der frischen Luft. Einfach registieren und loslegen.
+            Weiterhin werden unterschiedliche Veranstaltungen wie Vorträge, Spaziergänge ("Mapathons") und ein (digitaler) monatlicher Stammtisch zum Erfahrungsaustausch angeboten: informieren Sie sich dazu auf der <a href="https://colouring.dresden.ioer.info" target="_blank">Projektwebseite</a>.
         </p>
         <p>
-            Zum Projekt:
+            <b>Zum Projekt:</b>
         </p>
         <p>
             Die Plattform Colouring {config.cityName} wird im Rahmen des Citizen Science Projektes "Baukultur und klimagerechte Architektur in Dresden. Gebäudewissen kartieren, erforschen und vermitteln"
@@ -43,7 +43,7 @@ const Welcome = () => (
             um über die Entwicklung und Aktivitäten zu informieren.
         </p>
         <p>
-            Neue Datensätze oder Funktionen werden kontinuierlich ergänzt. Über Ihre Mitwirkung bei der Erfassung oder Verifikation von Gebäudemerkmalen sind wir dankbar!
+            Neue Datensätze oder Funktionen werden kontinuierlich ergänzt. Für Ihre Mitwirkung bei der Erfassung oder Verifikation von Gebäudemerkmalen sind wir dankbar!
         </p>
         <p>
             Die <Link to="/data-extracts.html">Daten</Link> sowie der <a href="https://github.com/colouring-cities/colouring-dresden" target="_blank">Programmcode</a> stehen frei zum Herunterladen unter offenen Lizenzen bereit. 
