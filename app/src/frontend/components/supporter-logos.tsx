44908e54e5103fd29033dd8cfa71d004d53e253d
traveller195 <theo92@gmx.net>
2023-03-03 20:28:51 +0100

    Adapt supporter logos tsx

--- app/src/frontend/components/supporter-logos.tsx
+++ app/src/frontend/components/supporter-logos.tsx
@@ -7,33 +7,43 @@ const SupporterLogos = () => (
         <h2 className="h3">Lead Organisation and Project Partners</h2>
         <ul className="logo-list">
             <li>
-                <a href="https://www.ucl.ac.uk/bartlett/casa/">
-                    <img src="images/logo-casa.png"
-                        alt="Centre for Advanced Spatial Analysis (CASA)" />
+                <a href="https://ioer.de/" target="_blank">
+                    <img className="cl-logo" src="images/logo-ioer-de.svg" alt="Leibniz-Institut für ökologische Raumentwicklung (IÖR) e.V."></img>
                 </a>
             </li>
             <li>
-                <a href="https://www.ucl.ac.uk/">
-                    <img src="images/logo-ucl.png"
-                        alt="University College London" />
+                <a href="https://pages.colouring.london/colouring-cities" target="_blank">
+                    <img className="cl-logo" src="images/logo-cc.jpg" alt="Colouring Cities Research Programme"></img>
                 </a>
             </li>
             <li>
-                <a href="https://www.historicengland.org.uk/">
-                    <img src="images/logo-he.png"
-                        alt="Historic England" />
+                <a href="https://www.citizenscience-wettbewerb.de/" target="_blank">
+                    <img className="cl-logo" src="images/AdP Banner Wortmarke 560x300 300dpi (3) (1).png" alt="Auf die Plätze! Citizen Science in deiner Stadt."></img>
                 </a>
             </li>
             <li>
-                <a href="https://www.ordnancesurvey.co.uk/">
-                    <img src="images/logo-os.png"
-                        alt="Ordnance Survey" />
+                <a href="https://www.wissenschaft-im-dialog.de/" target="_blank">
+                    <img className="cl-logo" src="images/01_LOGO_WID-rgb.png" alt="Wissenschaft im Dialog"></img>
                 </a>
             </li>
             <li>
-                <a href="https://www.london.gov.uk/">
-                    <img src="images/logo-gla.png"
-                        alt="Supported by the Mayor of London" />
+                <a href="https://www.museumfuernaturkunde.berlin/de" target="_blank">
+                    <img className="cl-logo" src="images/02_mfn_logo_STANDARD_auf_weiss-kl.png" alt="Museum für Naturkunde Berlin/ Leibniz-Institut für Evolutions- und Biodiversitätsforschung"></img>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.buergerschaffenwissen.de/" target="_blank">
+                    <img className="cl-logo" src="images/BsW_Logo-m-Claim-WEB-kl.png" alt="Bürger schaffen Wissen"></img>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.bmbf.de/" target="_blank">
+                    <img className="cl-logo" src="images/BMBF_gefoerdert_vom_deutsch_CMYK.png" alt="Bundesministerium für Bildung und Forschung"></img>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.bmbf.de/bmbf/de/ueber-uns/wissenschaftskommunikation-und-buergerbeteiligung/buergerbeteiligung/citizen-science/buergerforschung.html" target="_blank">
+                    <img className="cl-logo" src="images/BForschung_Logo_web-kl.png" alt="Bürgerforschung Wissenschaft für alle"></img>
                 </a>
             </li>
         </ul>
