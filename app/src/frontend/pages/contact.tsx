4c13d59846f00474ee43cd1546809213a4c18adf
traveller195 <theo92@gmx.net>
2023-03-03 22:26:32 +0100

    Translate contact tsx into german

--- app/src/frontend/pages/contact.tsx
+++ app/src/frontend/pages/contact.tsx
@@ -8,32 +8,24 @@ const ContactPage = () => (
     <article>
         <section className="main-col">
             <h1 className="h2">
-                Contact
+                Kontakt
             </h1>
-            <p> Colouring Cities has been designed as a sustainable, low-cost model for knowledge exchange/open data platforms able to be reproduced by other towns and cities using our open platform code.</p>
-
-            <p> It is being developed by a small, dedicated team at The Alan Turing Institute. We are unable to answer individual queries but welcome constructive comments on how to improve the site, and on other types of data and new features you might like to see.</p>
-
-            <p> You can send us comments or ask questions on our discussion threads at <a href="https://discuss.colouring.london/">https://discuss.colouring.london/</a>.</p>
-
-            <p> To view our technical site and platform code please visit Github at: <a href={ccconfig.githubURL}>https://github.com/colouring-cities/colouring-core</a>.</p>
-
-            <p>For press enquiries please contact <a href="https://www.turing.ac.uk/contact-us/press-office">The Alan Turing Institute press office</a></p>
-
             <p>
-            If you capture images from the maps on Colouring Cities, please credit our
-            contributors (who collected the data) and Ordnance Survey
-            (who provided the basemaps and building geometries) as follows:
+                <b>Projektwebseite "Colouring Dresden":</b> https://colouring.dresden.ioer.info
             </p>
             <p>
-            <pre><code>
-            Colouring Cities https://colouring.london Building attribute data is © Colouring Cities contributors. Maps contain OS data © Crown copyright: OS Maps baselayers and building outlines.
-            </code></pre>
+                <b>E-Mail:</b> colouringdresden@ioer.de
             </p>
-            <hr />
             <p>
-                <img className="d-block mx-auto" src="images/logo-cl.png"></img>
+                <b>Newsletter:</b> https://seu2.cleverreach.com/f/203678-351100/
             </p>
+            <p>
+                <b>Twitter:</b> https://twitter.com/colouringdd
+            </p>
+            <p>
+                <b>Instagram:</b> https://www.instagram.com/colouringdd/
+            </p>
+
         </section>
     </article>
 );
